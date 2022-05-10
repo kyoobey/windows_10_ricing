ad5df59754b75ad1fb116cd2dbecad0384ba42bf
tshrpl <tshrpl@protonmail.com>
2022-05-10 22:58:23 +0530

    fix secureux theme installer for paranoid android nebula

--- secureux_themes/theme_installers/paranoid_android_nebula.bat
+++ secureux_themes/theme_installers/paranoid_android_nebula.bat
@@ -1,4 +1,6 @@
 @echo off
-cp "..\themes\paranoid_android_by_niivu\nebula.theme" C:\Windows\Resources\Themes\
-cp "..\themes\paranoid_android_by_niivu\Paranoid Android\Nebula.msstyles" "C:\Windows\Resources\Themes\Paranoid Android"
-cp "..\themes\paranoid_android_by_niivu\Paranoid Android\Shell\NormalColor\shellstyle.dll" "C:\Windows\Resources\Themes\Paranoid Android\Shell\NormalColor\"
+copy "..\themes\paranoid_android_by_niivu\nebula.theme" C:\Windows\Resources\Themes\
+md "C:\Windows\Resources\Themes\Paranoid Android"
+copy "..\themes\paranoid_android_by_niivu\Paranoid Android\Nebula.msstyles" "C:\Windows\Resources\Themes\Paranoid Android"
+md "C:\Windows\Resources\Themes\Paranoid Android\Shell\NormalColor\"
+copy "..\themes\paranoid_android_by_niivu\Paranoid Android\Shell\NormalColor\shellstyle.dll" "C:\Windows\Resources\Themes\Paranoid Android\Shell\NormalColor\"
