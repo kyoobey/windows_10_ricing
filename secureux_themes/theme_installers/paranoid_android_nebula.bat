5f873d014e6e52afa8b715f88d36b48c116224c9
tshrpl <tshrpl@protonmail.com>
2022-05-10 13:53:04 +0530

    add installer for secureux theme paranoid android nebula

---
+++ secureux_themes/theme_installers/paranoid_android_nebula.bat
@@ -0,0 +1,4 @@
+@echo off
+cp "..\themes\paranoid_android_by_niivu\nebula.theme" C:\Windows\Resources\Themes\
+cp "..\themes\paranoid_android_by_niivu\Paranoid Android\Nebula.msstyles" "C:\Windows\Resources\Themes\Paranoid Android"
+cp "..\themes\paranoid_android_by_niivu\Paranoid Android\Shell\NormalColor\shellstyle.dll" "C:\Windows\Resources\Themes\Paranoid Android\Shell\NormalColor\"
